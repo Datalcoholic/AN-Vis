bb20faa091a83fb7c6fd66f9e51bd4a12207ce69
Datalcoholic <rechezuria@gmail.com>
2019-09-30 18:01:08 -0400

    Maping sobre array 1 y array 2

--- Data/flatten.js
+++ Data/flatten.js
@@ -1,4 +1,6 @@
 const fs = require('fs');
+const csv = require('csv-parser');
+const iconv = require('iconv-lite');
 
 // Extraer los elementos de cada diputado
 const raw = fs.readFileSync('dipData.json');
@@ -53,27 +55,65 @@ const dipFaltanMud = [
 
 const totalDipMud = [...mudDip, ...dipFaltanMud];
 
+// Completar la informacion de partido, condicion
+const dataMudDipRaw = [];
+
+fs.createReadStream('../Data/diputadosMud.csv')
+	.pipe(iconv.decodeStream('iso88591'))
+	.pipe(csv({ separator: ';' }))
+	.on('data', d => dataMudDipRaw.push(d))
+	.on('end', () => {
+		//console.log(dataMudDipRaw);
+		let match = [];
+		let noMatch = [];
+		totalDipMud.forEach(d => {
+			dataMudDipRaw.forEach(r => {
+				if (d.name.trim() === r.name.trim()) {
+					match.push({
+						name: d.name,
+						img: d.img,
+						condicion: d.condicion,
+						bancada: r.coalicion,
+						partido: r.partido,
+						circuncripcion: d.circuncripcion,
+						representante_de: d.representante_de,
+						estado: d.estado,
+						suplente: d.suplente,
+						estado_legal: r.estado_legal
+					});
+					console.log(
+						'total Dip',
+						d.name.trim(),
+						'total Dip json',
+						r.name.trim(),
+						'true'
+					);
+				}
+			});
+		});
+		console.log(match.length, 'de', dataMudDipRaw.length);
+		console.log(match);
+	});
+
 //TODO:
 // Completar info de diputados del PSUV
 
 // console.log(totalDipMud);
 // console.log('mudDip', mudDip.length, 'Vs', 'totalDipMud', totalDipMud.length);
-//TODO:
-// Completar la informacion de partido, condicion
 
 // Save totalDipMud
-fs.writeFileSync(
-	'Diputados_mud.csv',
-	'name,img ,condicion ,bancada ,circuncripcion,representante_d,estado ,suplente ,twitter ,instagram\n'
-);
+// fs.writeFileSync(
+// 	'Diputados_mud.csv',
+// 	'name,img ,condicion ,bancada ,circuncripcion,representante_d,estado ,suplente ,twitter ,instagram\n'
+// );
 
-totalDipMud
-	.sort((a, b) => a.condicion > b.condicion)
-	.forEach(d =>
-		fs.appendFileSync(
-			'Diputados_mud.csv',
-			`${d.name},${d.img} ,${d.condicion} ,${d.bancada} ,${d.circuncripcion},${d.representante_d},${d.estado} ,${d.suplente} ,${d.twitter} ,${d.instagram}\n`
-		)
-	);
+// totalDipMud
+// 	.sort((a, b) => a.condicion > b.condicion)
+// 	.forEach(d =>
+// 		fs.appendFileSync(
+// 			'Diputados_mud.csv',
+// 			`${d.name},${d.img} ,${d.condicion} ,${d.bancada} ,${d.circuncripcion},${d.representante_d},${d.estado} ,${d.suplente} ,${d.twitter} ,${d.instagram}\n`
+// 		)
+// 	);
 
 // Concatenar
